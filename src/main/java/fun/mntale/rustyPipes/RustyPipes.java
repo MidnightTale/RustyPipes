a01970db9ea1b9185005f82dce22cd4b041d20d0
MidnightTale <midnighttale24@gmail.com>
2025-07-04 08:28:04 +0700

    Init

---
+++ src/main/java/fun/mntale/rustyPipes/RustyPipes.java
@@ -0,0 +1,17 @@
+package fun.mntale.rustyPipes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RustyPipes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
